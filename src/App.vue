470c829174e9b9a92c6f64fbf5069760f7eabc18
panda <SmelayaPanda@mail.ru>
2018-02-21 14:48:09 +0700

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,33 @@
+<template>
+  <div id="app">
+    <app-header></app-header>
+    <router-view/>
+  </div>
+</template>
+
+<script>
+import AppHeader from './components/Header'
+
+export default {
+  components: {
+    AppHeader
+  },
+  name: 'App'
+}
+</script>
+
+<style>
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  #app {
+    font-family: 'Avenir', Helvetica, Arial, sans-serif;
+    -webkit-font-smoothing: antialiased;
+    -moz-osx-font-smoothing: grayscale;
+    text-align: center;
+    color: #2c3e50;
+
+  }
+</style>
---
+++ src/components/Header.vue
@@ -0,0 +1,17 @@
+<template>
+  <el-header height="60px" class="app-header">
+    Hi
+  </el-header>
+</template>
+
+<script>
+export default {
+  name: 'Header'
+}
+</script>
+
+<style scoped lang="sass">
+  .app-header
+    background-color: $color-primary
+
+</style>
